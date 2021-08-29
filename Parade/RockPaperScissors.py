0a999cc6fa6853fdebc8d153afc7711d5f7d1ca5
eroliver <eroliver27@hotmail.com>
2021-08-29 15:33:13 -0400

    Testing git in pycharm.

---
+++ Parade/RockPaperScissors.py
@@ -0,0 +1,18 @@
+import random
+
+def determine_outcome(player, computer):
+
+def rpsgame():
+    player_score = 0
+    computer_score = 0
+    moves = ["Rock", "Paper", "Scissors"]
+    player_move = input("Make your move: ").upper()
+    computer_move = random.choice(moves)
+    print("Computer's move: "+computer_move)
+    if(player_move == "ROCK"):
+        if(computer_move == "Rock"):
+            print("Rocks clack, no score.")
+        elif(computer_move == "Paper")
+            print("Paper covers Rock, computer p")
+
+rpsgame()
