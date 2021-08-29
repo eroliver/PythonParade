ab95e0462022da35d776b9c759b8e116d8739298
Eric Oliver <eroliver27@hotmail.com>
2021-08-29 17:08:37 -0400

    testing update, pyvenv

--- Parade/RockPaperScissors.py
+++ Parade/RockPaperScissors.py
@@ -1,6 +1,9 @@
 import random
 
+
 def determine_outcome(player, computer):
+    pass
+
 
 def rpsgame():
     player_score = 0
@@ -8,11 +11,12 @@ def rpsgame():
     moves = ["Rock", "Paper", "Scissors"]
     player_move = input("Make your move: ").upper()
     computer_move = random.choice(moves)
-    print("Computer's move: "+computer_move)
-    if(player_move == "ROCK"):
-        if(computer_move == "Rock"):
+    print("Computer's move: " + computer_move)
+    if (player_move == "ROCK"):
+        if (computer_move == "Rock"):
             print("Rocks clack, no score.")
-        elif(computer_move == "Paper")
+        elif (computer_move == "Paper"):
             print("Paper covers Rock, computer p")
+
 
 rpsgame()
