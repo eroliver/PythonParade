1115d4b3b5f899984cb9ce48f5fdddbc940b03a3
Eric Oliver <eroliver27@hotmail.com>
2021-08-30 22:16:12 -0400

    Finished windows wall paper updater script. Cycles through local directory of images randomly and sets the selected image as the wall paper.

--- Parade/CycleWallPaper.py
+++ Parade/CycleWallPaper.py
@@ -1,11 +1,18 @@
 import ctypes
-import time
+import random
+from time import time, sleep
 
 
-def cycle_wallpapers():
-    pass
+def cycle_wallpapers(images):
+    image_list = [i for i in range(1, images + 1)]
+    chosen_bg = random.choice(image_list)
+    image_name = f'BG ({chosen_bg})'
+    image_folder_path = f'E:\Projects\PycharmProjects\PythonParade\Parade\images\{image_name}.jpg'
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, image_folder_path, 0)
+    print(image_folder_path)
 
 
-image_folder_path = 'E:\Projects\PycharmProjects\PythonParade\Parade\images'
-image_name = '\sample_name'
-ctypes.windll.user32.SystemParametersInfoW(20, 0, image_folder_path + image_name, 0)
+while True:
+    sleep(5 - time() % 5)
+    cycle_wallpapers(22)
+
