4d8b115888e921048c99d257b04e7fede15d8581
Eric Oliver <eroliver27@hotmail.com>
2021-09-02 00:09:47 -0400

    Pulling info from indeed job cards, working on ignoring the new tag that is placed above some jobs.

--- Parade/IndeedScraper.py
+++ Parade/IndeedScraper.py
@@ -1,3 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def extract(page):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/93.0.4577.63 Safari/537.36'}
+    url = f'https://www.indeed.com/jobs?q=junior%20developer&l=Remote&start={page}&vjk=1d22fae19e010810'
+    r = requests.get(url, headers)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup
+
+
+def transform(soup):
+    divs = soup.find_all('div', class_='slider_container')
+    for div in divs:
+        if div.span['title'] is not None:
+            title = div.span['title']
+        print(title)
+    return
+
+
+c = extract(0)
+transform(c)
