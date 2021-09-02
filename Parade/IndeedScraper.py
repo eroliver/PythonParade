19ccf7c880a4406c24641f38aa025b031e0bb2e3
eroliver <eroliver27@hotmail.com>
2021-09-02 18:43:53 -0400

    Fixed bug made by the indeed new job tag. Added company name and salary to results.

--- Parade/IndeedScraper.py
+++ Parade/IndeedScraper.py
@@ -4,19 +4,42 @@ from bs4 import BeautifulSoup
 
 def extract(page):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                             'Chrome/93.0.4577.63 Safari/537.36'}
-    url = f'https://www.indeed.com/jobs?q=junior%20developer&l=Remote&start={page}&vjk=1d22fae19e010810'
+                             'Chrome/92.0.4515.159 Safari/537.36'}
+    url = f'https://www.indeed.com/jobs?q=junior%20developer&l=Remote&start=10{page}&vjk=1d22fae19e010810'
     r = requests.get(url, headers)
     soup = BeautifulSoup(r.content, 'html.parser')
     return soup
+# home user agent Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+#                             'Chrome/93.0.4577.63 Safari/537.36
+# work user agent Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+#                              'Chrome/92.0.4515.159 Safari/537.36
+
 
 
 def transform(soup):
     divs = soup.find_all('div', class_='slider_container')
+    # for testing 1 post at a time
+    # div = divs[8]
+    # company_name = div.find('span', class_='companyName').text.strip()
+    # spans = div.find_all('span')
+    # for span in spans:
+    #     attributes = span.attrs
+    #     if 'title' in attributes:
+    #         job_title = attributes['title'].strip()
+    # print(f'{job_title} : {company_name}')
+
     for div in divs:
-        if 'title' in div.span.attrs:
-            print(div.span.attrs['title'])
-        #print(title)
+        company_name = div.find('span', class_='companyName').text.strip()
+        spans = div.find_all('span')
+        for span in spans:
+            attributes = span.attrs
+            if 'title' in attributes:
+                job_title = attributes['title'].strip()
+        try:
+            salary = div.find('span', class_='salary-snippet').text.strip()
+        except:
+            salary = 'Not Listed'
+        print(f'{job_title} : {company_name} : {salary}')
     return
 
 
