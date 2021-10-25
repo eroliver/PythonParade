58a5e0f2ad9f822c9755e54033e8cdc495eb4f53
Eric Oliver <eroliver27@hotmail.com>
2021-10-25 02:20:31 -0400

    Started on this kiosk restarting app. Kivy buttons are weird. Used subprocess instead of importing os...apparently thats how it's done now. Need refresher on passing python arguments to clean up this code, but otherwise it works, I guess.

--- Parade/KivyApp/main.py
+++ Parade/KivyApp/main.py
@@ -1,14 +1,41 @@
-from kivy.app import App
+import kivy
 from kivy.uix.widget import Widget
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+import subprocess
 
 
-class MainWidget(Widget):
-    pass
+class SayHello(App):
+    def build(self):
+        self.window = GridLayout()
+        self.window.cols = 4
+        # add widgets to window
+        self.test_label = Label(text='Kiosk1')
+        self.window.add_widget(self.test_label)
+
+        self.button1 = Button(text='Kiosk1')
+        self.window.add_widget(self.button1)
+        self.system_name = 'mtrmgtkiosk1'
+        self.button1.bind(on_press=self.button_callback)
+
+        self.button2 = Button(text='Kiosk2')
+        self.window.add_widget(self.button2)
+        # the following line calls the print function on startup..NEED to pass a function for .bind to work
+        # self.button2.bind(on_press=print('button pressed'))
+
+        self.button3 = Button(text='Kiosk3')
+        self.window.add_widget(self.button3)
+
+        return self.window
+
+    # obviously this only works for 1 kiosk and would need to be passed the system name, but that didn't work.
+    def button_callback(self, system):
+        subprocess.run('ping mtrmgtkiosk1')
 
 
-class DadJokeApp(App):
-    pass
-
-
-DadJokeApp().run()
-
+if __name__ == "__main__":
+    SayHello().run()
