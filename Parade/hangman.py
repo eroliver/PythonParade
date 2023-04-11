e92992e2e6d55dd9134872b710447670dbbc61ff
eroliver <eroliver27@hotmail.com>
2023-04-11 01:49:36 -0400

    Added single player game loop using word lists imported from scowl. Used Getpass to hide 2 player secret word inupt. Another good setting to allow players to choose would be to change case sensitivity, currently c does not match C.

--- Parade/hangman.py
+++ Parade/hangman.py
@@ -1,7 +1,49 @@
+from random import randint
+from os import listdir, getcwd, path
+from getpass import getpass
 
-
-mistakesRemaining = 6
+mistakesMade = 0
 letters = []
+parentFolder = "word_list"
+fileNames = listdir(parentFolder)
+fileList = [(path.join(getcwd(), parentFolder, fileName)) for fileName in fileNames]
+
+HANGMAN_PICS = ['''
+  3.   +---+
+  4.       |
+  5.       |
+  6.       |
+  7.      ===''', '''
+  8.   +---+
+  9.   O   |
+ 10.       |
+ 11.       |
+ 12.      ===''', '''
+ 13.   +---+
+ 14.   O   |
+ 15.   |   |
+ 16.       |
+ 17.      ===''', '''
+ 18.   +---+
+ 19.   O   |
+ 20.  /|   |
+ 21.       |
+ 22.      ===''', '''
+ 23.   +---+
+ 24.   O   |
+ 25.  /|\  |
+ 26.       |
+ 27.      ===''', '''
+ 28.   +---+
+ 29.   O   |
+ 30.  /|\  |
+ 31.  /    |
+ 32.      ===''', '''
+ 33.   +---+
+ 34.   O   |
+ 35.  /|\  |
+ 36.  / \  |
+ 37.      ===''']
 
 def main():
     # Ask how many players
@@ -9,27 +51,35 @@ def main():
 
     if numberOfPlayers == 2:
         TwoPlayerLoop()
-
-
+    elif numberOfPlayers == 1:
+        SinglePlayerLoop()
+    else:
+        print("Sorry, but only 1 and 2 player versions are available.")
 
 def TwoPlayerLoop():
-    global mistakesRemaining
-    secretWord = input("Player 1, please enter the word for player 2 to guess: ")
-    while(mistakesRemaining > 0):
+    global mistakesMade
+    print("Player 1, please enter the word for player 2 to guess: ")
+    secretWord = getpass()
+    while(mistakesMade < 6):
 
-        print("Player 2, you have {} guesses remaining.".format(mistakesRemaining))
+        print("Player 2, you have {} guesses remaining.".format(6 - mistakesMade))
         letterGuessed = input("Player 2 please guess a letter: ")
         letters.append(letterGuessed)
         if letterGuessed in secretWord:
             print("Correct!")
         else:
             print("Incorrect!")
-            mistakesRemaining = mistakesRemaining - 1
+            mistakesMade += 1
 
-        print(GetHiddenstring(secretWord, letters))
+        print(letters)
+        encodedWord = GetHiddenstring(secretWord, letters)
+        print(encodedWord)
+        print(HANGMAN_PICS[mistakesMade])
+        if '*' not in encodedWord:
+            break
 
-    if mistakesRemaining <= 0:
-        print("Congratulations Player 1!")
+    if mistakesMade >= 6:
+        print("Congratulations Player 1! The word was {}!".format(secretWord))
     else:
         print("Congratulations Player 2!")
     
@@ -39,6 +89,45 @@ def  GetHiddenstring(word: str, letters: list):
         if letter not in letters:
             encodedWord = encodedWord.replace(letter, "*")
     return encodedWord
+
+def SinglePlayerLoop():
+    global mistakesMade
+    secretWord = GetSecretWord()
+    while(mistakesMade < 6):
+
+        print("Player, you have {} guesses remaining.".format(6 - mistakesMade))
+        letterGuessed = input("Please guess a letter: ")
+        letters.append(letterGuessed)
+        if letterGuessed in secretWord:
+            print("Correct!")
+        else:
+            print("Incorrect!")
+            mistakesMade += 1
+
+        print(letters)
+        encodedWord = GetHiddenstring(secretWord, letters)
+        print(encodedWord)
+        print(HANGMAN_PICS[mistakesMade])
+        if '*' not in encodedWord:
+            break
+
+    if mistakesMade >= 6:
+        print("Too Bad! The word was {}!".format(secretWord))
+    else:
+        print("Congratulations Player!")
+    
+def  GetHiddenstring(word: str, letters: list):
+    encodedWord = word
+    for letter in word:
+        if letter not in letters:
+            encodedWord = encodedWord.replace(letter, "*")
+    return encodedWord
+
+def GetSecretWord():
+    wordList = fileList[randint(0, len(fileList))]
+    with open(wordList,'r') as file:
+        words = file.readlines()
+    return((words[randint(0,len(words))]).strip())
 
 if __name__ == "__main__":
     main()
